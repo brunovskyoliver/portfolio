8c9117d81548a3808dba49af39d0d4e68188764b
Oliver Brunovsky <obrunovsky7@gmail.com>
2023-03-29 08:28:59 +0200

    huge

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -8,6 +8,7 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
     <div>
+      <Footer />
       <Navbar />
       <Blob />
       
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,6 @@ import Footer from './Footer';
 const Index = () => {
   return (
     <Layout>
-      <Footer />
       <Home />
       <About />
     </Layout>
