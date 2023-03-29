527df91d1e51fd1c7225f5b9e635c13d8bd31d0f
Oliver Brunovsky <obrunovsky7@gmail.com>
2023-03-29 08:35:47 +0200

    huge

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer(): JSX.Element {
     <div className="block fixed bottom-0 w-full h-4 z-10 left-0">
         <div className="flex justify-center items-center h-full">
             <p className="text-xs text-white font-mono">
-                Made by <a href='https://github.com/brunovskyoliver/' target="_blank" rel='noopener'>Oliver Brunovský </a><span className="text-red-500">❤</span>
+                Made by <a href='https://github.com/brunovskyoliver/' className='underline' target="_blank" rel='noopener'>Oliver Brunovský</a><span className="text-red-500"> ❤</span>
             </p>
         </div>
     </div>
