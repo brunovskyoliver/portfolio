813a9a6c3c73fdd7bb533ee6608d09d0b71208d9
Oliver Brunovsky <obrunovsky7@gmail.com>
2023-04-03 09:55:51 +0200

    login update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,36 @@
-import { ClerkProvider } from '@clerk/nextjs';
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from "@clerk/nextjs";
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import "./Blob.css"
+import { useRouter } from "next/router";
+
+const publicPages = ["/SignInPage/[[...index]]", "/SignUpPage/[[...index]]"];
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
+
+  // Check if the current route matches a public page
+  const isPublicPage = publicPages.includes(pathname);
+
   return (
-    <ClerkProvider>
-      <Component {...pageProps} />
+    <ClerkProvider {...pageProps}>
+      {isPublicPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </>
+      )}
     </ClerkProvider>
   );
 }
