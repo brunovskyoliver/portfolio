2f90215a86a44095784725b0597ff18a80f085e8
Oliver Brunovsky <obrunovsky7@gmail.com>
2023-04-04 10:25:01 +0200

    quality of life upd

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -23,65 +23,33 @@ const Navbar = () => {
   return (
     <nav className="flex justify-center py-6 z-10">
       <div className='flex justify-center'>
-      
       <ul className="flex items-center space-x-4 z-10">
         <li>
           <Link href="/">
-            <div
-              className={`text-white cursor-pointer z-10 ${
-                router.pathname === '/' ? 'font-bold' : ''
-              }`}
-            >
-              Home
-            </div>
+            <div className={`text-white cursor-pointer z-10 ${router.pathname === '/' ? 'font-bold' : ''}`}>Home</div>
           </Link>
         </li>
         <li>
           <Link href="/About">
-            <div
-              className={`text-white cursor-pointer z-10 ${
-                router.pathname === '/About' ? 'font-bold' : ''
-              }`}
-            >
-              About
-            </div>
+            <div className={`text-white cursor-pointer z-10 ${router.pathname === '/About' ? 'font-bold' : ''}`}>About</div>
           </Link>
         </li>
         <li>
           <Link href="/ContactMe">
-            <div
-              className={`text-white cursor-pointer z-10 ${
-                router.pathname === '/ContactMe' ? 'font-bold' : ''
-              }`}
-            >
-              Contact
-            </div>
+            <div className={`text-white cursor-pointer z-10 ${router.pathname === '/ContactMe' ? 'font-bold' : ''}`}>Contact</div>
           </Link>
         </li>
         <SignedOut>
         <li>
           <Link href="/SignUpPage">
-            <div
-              className={`text-white cursor-pointer z-10 ${
-                router.pathname === '/SignUpPage' ? 'font-bold' : ''
-              }`}
-            >
-              
-        Sign Up
-            </div>
+            <div className={`text-white cursor-pointer z-10 ${router.pathname === '/SignUpPage' ? 'font-bold' : ''}`}>Sign Up</div>
           </Link>
         </li>
         </SignedOut>
         <SignedOut>
         <li>
           <Link href="/SignInPage">
-            <div
-              className={`text-white cursor-pointer z-10 ${
-                router.pathname === '/SignInPage' ? 'font-bold' : ''
-              }`}
-            >
-        Sign In
-            </div>
+            <div className={`text-white cursor-pointer z-10 ${router.pathname === '/SignInPage' ? 'font-bold' : ''}`}>Sign In</div>
           </Link>
         </li>
         </SignedOut>
@@ -89,13 +57,7 @@ const Navbar = () => {
         <li>
         { isUserInOrganization ?
           <Link href="/Notes">
-            <div
-              className={`text-white cursor-pointer z-10 ${
-                router.pathname === '/Notes' ? 'font-bold' : ''
-              }`}
-            >
-              Notes
-            </div>
+            <div className={`text-white cursor-pointer z-10 ${router.pathname === '/Notes' ? 'font-bold' : ''}`}>Notes</div>
           </Link> : null}
           </li>
           <li>
@@ -103,8 +65,6 @@ const Navbar = () => {
         </li>
       </SignedIn>
       </ul>
-      
-
       <style jsx>{`
         nav {
           width: 100%;
