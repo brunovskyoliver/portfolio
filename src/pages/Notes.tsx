a95a08d7c195192790a6a802c538d72180a20c6f
Oliver Brunovsky <obrunovsky7@gmail.com>
2023-04-05 21:05:40 +0200

    notes update ui

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -11,10 +11,9 @@ export default function Notes(): JSX.Element {
   const orgId = "org_2NwqhR3YqilSddAl89q4g9dcRMY";
   const isUserInOrganization = user?.organizationMemberships.some(membership => membership.organization.id === orgId);
   if (isUserInOrganization) {
-    console.log("User is in organization");
+    console.log("user");
   }
   else {
-    console.log("User is not in organization");
     router.push("/");
     }
 
@@ -23,7 +22,36 @@ export default function Notes(): JSX.Element {
       <Head>
         <title>Notes - brunovsky.online</title>
       </Head>
-      
+      <main className="z-10">
+        <section className='border-2 border-solid border-white rounded-2xl w-full h-fit z-10 relative'>
+          <div className="flex flex-row overflow-hidden rounded-2xl w-full h-60 bg-black">
+            <div className=" overflow-hidden w-1/3 flex flex-col ">
+              <div className="h-1/2 w-full justify-center text-center flex items-center">
+                <p className="text-2x1 font-mono">A</p>
+              </div>
+              <div className="h-1/2 w-full justify-center text-center flex items-center">
+              <p className="text-2x1 font-mono">A</p>
+              </div>
+            </div>
+            <div className=" overflow-hidden w-1/3 flex flex-col">
+            <div className="h-1/2 w-full justify-center text-center flex items-center">
+            <p className="text-2x1 font-mono">A</p>
+            </div>
+            <div className="h-1/2 w-full justify-center text-center flex items-center">
+            <p className="text-2x1 font-mono">A</p>
+            </div>
+            </div>
+            <div className=" overflow-hidden w-1/3 flex flex-col ">
+            <div className="h-1/2 w-full justify-center text-center flex items-center">
+            <p className="text-2x1 font-mono">A</p>
+            </div>
+            <div className="h-1/2 w-full justify-center text-center flex items-center">
+            <p className="text-2x1 font-mono">A</p>
+            </div>
+            </div>
+          </div>
+        </section>
+      </main>
 
   </Layout>
   );
